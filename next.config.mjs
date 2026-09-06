b5ba48b7312273feece6b50d6a7166b734bb38ff
Roberto Pires <romariopesefootball@gmail.com>
2026-09-07 00:54:00 +0530

    feat: add next.config.mjs with Unsplash remote patterns

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
